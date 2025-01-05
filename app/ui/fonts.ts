4c35d61b41c64b19d9b11217549a7ccbd4c2a6d7
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-01-04 23:02:17 -0600

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Montserrat, Lusitana} from 'next/font/google'
+
+export const montserrat = Montserrat({subsets:['latin'], weight:['100','200','400']})
+
+export const lusitana = Lusitana({subsets:['latin'],weight:['400','700']})
