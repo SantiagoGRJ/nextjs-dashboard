6d5550878313269f5137a82919d4ce45abf7ed92
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-01-08 20:08:59 -0600

    Add Register Invoice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,22 +6,23 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   
-  const searchParams = useSearchParams() // acceder en modo lectura al query que esta en la URL
-  const pathname = usePathname() // acceder en la ruta que estamos
-  const { replace } = useRouter() 
+  const searchParams = useSearchParams()
+  const pathName = usePathname()
+  const { replace } = useRouter()
 
- const handleSearch = useDebouncedCallback((term) => {
-  
-  
-  const params = new URLSearchParams(searchParams) // convertir la URL a una clase para trabajar con ella. 
-  params.set('page','1')
-  if(term){
-   params.set('query',term)
-  }else{
-   params.delete('query')
-  }
-  replace(`${pathname}?${params.toString()}`)
- },300)
+  const handleSearch = useDebouncedCallback((term:string) => {
+    
+    const params = new URLSearchParams(searchParams)
+    params.set('page','1')
+    if(term){
+      params.set('query',term)
+    }else{
+      params.delete('query')
+    }
+    replace(`${pathName}?${params.toString()}`)
+
+
+  },300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -31,13 +32,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={
-          (e) => {
-            handleSearch(e.target.value)
-          }
+       onChange={
+        (e) => {
+          handleSearch(e.target.value)
         }
-        defaultValue={searchParams.get('query')?.toString()}
-        
+       }
+       defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
