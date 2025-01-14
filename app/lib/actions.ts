f6ca1289125a71d3d47bdb8bfc0f4723fe08d87b
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-01-14 16:09:53 -0600

    Fixed function deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,9 +113,9 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-   // return { message: "Deleted Invoice" };
-  } catch (error) {
-   // return { message: "Database Error: Failed to Delete Invoice " + error };
+    //return { message: "Deleted Invoice" };
+  } catch (_error) {
+    //return { message: "Database Error: Failed to Delete Invoice " + error };
   }
 }
 
